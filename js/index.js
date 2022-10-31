a57cf47505100609ed5818dfa41ba7d5cdba7608
fusejung <dlwlsrl468@gmail.com>
2022-10-31 18:03:03 +0900

    221031 수정

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
 
 
+
   $('.mainmenu>li').mouseover(function(){
     $(this).find('.submenu').stop().slideDown(500);
     $('.bg').stop().slideDown(500);
@@ -29,11 +30,31 @@ $(document).ready(function(){
     $(".input-box__placeholder > label").removeClass("focus");
   });
 
+  
+  $("#search2").focusin(function(){
+    $(".input-box__placeholder2 > label").addClass("focus");
+  });
+  
+  $("#search2").focusout(function(){
+    $(".input-box__placeholder2 > label").removeClass("focus");
+  });
 
 
 
+  setInterval(function(){
+    $('.ad-slides').animate({'margin-left':'-1200px'},function(){
+      $('.ad-slide').first().appendTo('.ad-slides')
+      $('.ad-slides').css({'margin-left':'0'})
+    })
+  },5000)
 
-
+  // $(".product-box").slick({
+  //   prevArrow:
+  //     "<div class='btn-prev'><i class='fa-solid fa-angle-left'></i></div>",
+  //   nextArrow:
+  //     "<div class='btn-next'><i class='fa-solid fa-angle-right'></i></div>",    
+  // });
+      
 
 
 
